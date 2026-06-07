4ed552e9fbd536028260a92a976c3aff9e67ba5d
THEMEZE <guillaumethemeze@gmail.com>
2026-06-07 19:54:43 +0200

    Quartz sync: Jun 7, 2026, 7:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Guillaume THÉMÈZE",
+    pageTitle: "Traveaux",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "fr-FR",
-    baseUrl: "THEMEZE.github.io/quartz_note",
+    baseUrl: "Lycee3B.github.io/quartz_note",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
